d189027066c9c409b6d7b46ce373d3bfd9ad76d0
Golang <tambolisumera@gmail.com>
2025-06-23 18:29:23 +0530

    Add new line

--- SecondMain.go
+++ SecondMain.go
@@ -1,7 +1,77 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
+
+// func main() {
+// 	fmt.Println(1 +1-10)
+// 	fmt.Println("sumera tamboli")
+// // }
+// package main
+// import ("fmt")
+
+// func main() {
+//   fmt.Println("Hello World!")
+//   // fmt.Println("This line does not execute")
+// // }
+// package main
+// import ("fmt")
+
+// func main() {
+//   var student1 string = "John" //type is string
+//   var student2 = "Jane" //type is inferred
+//   x := 2 //type is inferred
+
+//   fmt.Println(student1)
+//   fmt.Println(student2)
+//   fmt.Println(x)
+// }
+
+// package main
+// import ("fmt")
+
+// func main() {
+//   var a string
+//   var b int
+//   var c bool
+
+//   fmt.Println(a)
+//   fmt.Println(b)
+//   fmt.Println(c)
+// }
+// package main
+// import ("fmt")
+
+// func main() {
+//   var a, b, c, d int = 1, 3, 5, 7
+
+//   fmt.Println(a)
+//   fmt.Println(b)
+//   fmt.Println(c)
+//   fmt.Println(d)
+// // }
+// package main
+// import ("fmt")
+
+// func main() {
+//   var i,j string = "Hello","World"
+
+//   fmt.Print(i)
+//   fmt.Print(j)
+// }
+// 
+
+package main
+import ("fmt")
 
 func main() {
-	fmt.Println("Sumera Tamboli")
+  var a bool = true     // Boolean
+  var b int = 5         // Integer
+  var c float32 = 3.14  // Floating point number
+  var d string = "Hi!"  // String
+
+  fmt.Println("Boolean: ", a)
+  fmt.Println("Integer: ", b)
+  fmt.Println("Float:   ", c)
+  fmt.Println("String:  ", d)
 }
+
