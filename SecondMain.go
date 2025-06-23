f313c965d09bf15380c84a2d4a50d7a9757cd745
Golang <tambolisumera@gmail.com>
2025-06-23 18:41:38 +0530

    add new line

--- SecondMain.go
+++ SecondMain.go
@@ -60,18 +60,92 @@
 // }
 // 
 
+// package main
+// import ("fmt")
+
+// func main() {
+//   var a bool = true     // Boolean
+//   var b int = 5         // Integer
+//   var c float32 = 3.14  // Floating point number
+//   var d string = "Hi!"  // String
+
+//   fmt.Println("Boolean: ", a)
+//   fmt.Println("Integer: ", b)
+//   fmt.Println("Float:   ", c)
+//   fmt.Println("String:  ", d)
+// }
+
+// 
+
+// package main
+// import ("fmt")
+
+// func main() {
+//   myslice1 := []int{}
+//   fmt.Println(len(myslice1))
+//   fmt.Println(cap(myslice1))
+//   fmt.Println(myslice1)
+
+//   myslice2 := []string{"Go", "Slices", "Are", "Powerful"}
+//   fmt.Println(len(myslice2))
+//   fmt.Println(cap(myslice2))
+//   fmt.Println(myslice2)
+// }
+
+// package main
+// import ("fmt")
+
+// func main() {
+//   var (
+//     sum1 = 100 + 50 // 150 (100 + 50)
+//     sum2 = sum1 + 250 // 400 (150 + 250)
+//     sum3 = sum2 + sum2 // 800 (400 + 400)
+//   )
+//   fmt.Println(sum3)
+// }
+// package main
+// import ("fmt")
+
+// func main() {
+//   day := 4
+
+//   switch day {
+//   case 1:
+//     fmt.Println("Monday")
+//   case 2:
+//     fmt.Println("Tuesday")
+//   case 3:
+//     fmt.Println("Wednesday")
+//   case 4:
+//     fmt.Println("Thursday")
+//   case 5:
+//     fmt.Println("Friday")
+//   case 6:
+//     fmt.Println("Saturday")
+//   case 7:
+//     fmt.Println("Sunday")
+//   }
+// }
+
+// package main
+// import ("fmt")
+
+// func main() {
+//   for i:=0; i <= 100; i+=10 {
+//     fmt.Println(i)
+//   }
+// }
+
+// 
+// 
+
 package main
 import ("fmt")
 
 func main() {
-  var a bool = true     // Boolean
-  var b int = 5         // Integer
-  var c float32 = 3.14  // Floating point number
-  var d string = "Hi!"  // String
+  var a = map[string]string{"brand": "Ford", "model": "Mustang", "year": "1964"}
+  b := map[string]int{"Oslo": 1, "Bergen": 2, "Trondheim": 3, "Stavanger": 4}
 
-  fmt.Println("Boolean: ", a)
-  fmt.Println("Integer: ", b)
-  fmt.Println("Float:   ", c)
-  fmt.Println("String:  ", d)
+  fmt.Printf("a\t%v\n", a)
+  fmt.Printf("b\t%v\n", b)
 }
-
